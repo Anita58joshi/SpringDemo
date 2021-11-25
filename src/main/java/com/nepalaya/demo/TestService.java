05e15b1d6b78a4c1ff7567c59690ce31aff6a043
unknown <you@example.com>
2021-11-25 15:10:36 +0545

    springbegin

---
+++ src/main/java/com/nepalaya/demo/TestService.java
@@ -0,0 +1,11 @@
+package com.nepalaya.demo;
+
+public class TestService {
+    public TestService(){
+        System.out.println("TestService is constructed");
+    }
+
+    public void initMethod(){
+        System.out.println("TestService initMethod is triggered");
+    }
+}
